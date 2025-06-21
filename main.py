23df6d4245a3d2a4945e64cb22dfcbdf0c61435c
Vinay4321553 <SAHUVINAY806@GMAIL.COM>
2025-06-21 11:07:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+#import pickle 
+
+#movies = pickle.load(open("movies_list.pkl", 'rb'))
+
+
+st.header("Movie Recommender System")
+#st.selectbox("Select movie from dropdown", movies)
+
+#if st.button("show Recommend"):
+    #pass
